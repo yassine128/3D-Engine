9d1a19a61e2d6078839d26ba6012c8ae1462b7fe
yassine128 <yassineseddaoui@gmail.com>
2024-01-17 18:28:35 -0500

    Reading obj file

---
+++ Blender Clone/main.cpp
@@ -0,0 +1,193 @@
+#include <cstddef>
+#include <cstdint>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <glbinding/Binding.h>
+#include <glbinding/gl/gl.h>
+#include <SFML/Window.hpp>
+
+
+using namespace gl;
+
+int maxValue = 10; 
+
+
+// Quelques couleurs en format RGB réel (0 à 1).
+float grey[] = { 0.5f, 0.5f, 0.5f };
+float white[] = { 1.0f, 1.0f, 1.0f };
+float brightRed[] = { 1.0f, 0.2f, 0.2f };
+float brightGreen[] = { 0.2f, 1.0f, 0.2f };
+float brightBlue[] = { 0.2f, 0.2f, 1.0f };
+float brightYellow[] = { 1.0f, 1.0f, 0.2f };
+
+std::vector<std::vector<float>> colors = {
+		{0.5f, 0.5f, 0.5f},     // grey
+		{1.0f, 1.0f, 1.0f},     // white
+		{1.0f, 0.2f, 0.2f},     // brightRed
+		{0.2f, 1.0f, 0.2f},     // brightGreen
+		{0.2f, 0.2f, 1.0f},     // brightBlue
+		{1.0f, 1.0f, 0.2f}      // brightYellow
+};
+
+struct Vertex {
+	float x, y, z;
+};
+
+struct Face {
+	int v1, v2, v3;
+};
+
+void drawTriangle(Vertex v1, Vertex v2, Vertex v3, std::vector<float>& color) {
+	glColor3fv(color.data());
+	glBegin(GL_TRIANGLES); {
+		glVertex3f(v1.x * maxValue, v1.y * maxValue, v1.z * maxValue);
+		glVertex3f(v2.x * maxValue, v2.y * maxValue, v2.z * maxValue);
+		glVertex3f(v3.x * maxValue, v3.y * maxValue, v3.z * maxValue);
+	} glEnd();
+}
+
+
+void drawObject(std::vector<Face> faces, std::vector<Vertex> vertices) {
+	int index = 0;
+	for (Face face : faces) {
+		drawTriangle(vertices[face.v1 - 1], vertices[face.v2 - 1], vertices[face.v3 - 1], colors[index % 6]);
+		index++;
+	}
+}
+
+
+std::vector<Vertex> parseFile(std::string filename, std::vector<Face>& faces) {
+	std::vector<Vertex> vertices;
+
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return vertices; // Return empty vector on error
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		std::istringstream iss(line);
+		std::string type;
+		iss >> type;
+
+		if (type == "v") {
+			Vertex v;
+			iss >> v.x >> v.y >> v.z;
+			//maxValue = std::max((int)std::max(std::max(v.x, v.y), v.z), maxValue);
+
+			vertices.push_back(v);
+		}
+		else if (type == "f") {
+			Face f1;
+			Face f2;
+			std::vector<int> indices;
+			std::string token;
+
+			while (iss >> token) {
+				size_t pos = token.find('/');
+				if (pos != std::string::npos) {
+					// Extract the vertex index and convert to integer
+					int index = std::stoi(token.substr(0, pos));
+					indices.push_back(index);
+				}
+			}
+
+			switch (indices.size()) {
+			case 3: 
+				f1.v1 = indices[0];
+				f1.v2 = indices[1];
+				f1.v3 = indices[2];
+				faces.push_back(f1);
+				break; 
+			case 4: 
+				f1.v1 = indices[0];
+				f1.v2 = indices[1];
+				f1.v3 = indices[2];
+				faces.push_back(f1);
+				f2.v1 = indices[0];
+				f2.v2 = indices[2];
+				f2.v3 = indices[3];
+				faces.push_back(f2);
+				break; 
+			}
+			
+		}
+	}
+
+	file.close();
+	return vertices;
+}
+
+int main(int argc, char* argv[]) {
+	std::string filename = "C:\\Users\\Yassine\\Documents\\Personnal Cpp Projects\\Blender Clone\\Blender Clone\\Cube.obj";
+	std::vector<Face> faces;
+	std::vector<Vertex> objVertices = parseFile(filename, faces);
+
+	/*for (const auto& vertex : objVertices) {
+		std::cout << "Vertex: (" << vertex.x << ", " << vertex.y << ", " << vertex.z << ")\n";
+	}
+
+	for (const auto& face : faces) {
+		std::cout << "FACE: (" << face.v1 << ", " << face.v2 << ", " << face.v3 << ")\n";
+	}*/
+
+
+
+	sf::Window window;
+	window.create(
+		{ 800, 800 },            
+		"Blender Clone",		
+		sf::Style::Default,     
+		sf::ContextSettings(24)
+	);
+	window.setFramerateLimit(30);
+	window.setActive(true);
+	glbinding::Binding::initialize(nullptr, true); 
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1, 10);
+	glTranslatef(0, 0, -6);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glEnable(GL_DEPTH_TEST);
+
+	glClearColor(0.05f, 0.1f, 0.1f, 1.0f);
+
+	glEnable(GL_LINE_SMOOTH);
+	glLineWidth(5.0f);
+	glEnable(GL_POINT_SMOOTH);
+	glPointSize(5.0f);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	// Boucle d'exécution
+	while (window.isOpen()) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glRotatef(1, 1, 1, 0);
+
+		// Commentez/décommentez chacune des lignes pour tester chaque affichage.
+		// drawWhiteTriangle();
+		// drawColoredTriangle();
+		// drawPyramid();
+		drawObject(faces, objVertices); 
+
+		window.display();
+		sf::Event e;
+		while (window.pollEvent(e)) {
+			if (e.type == sf::Event::EventType::Closed)
+				window.close();
+		}
+	}
+	
+
+	return 0;
+}
+
